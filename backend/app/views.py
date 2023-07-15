6a5dc4d429b875c703dc8e15b545c1e3103545ad
ShishirSap <shishir.sap83@gmail.com>
2023-07-15 18:31:24 +0545

    requirements

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps=PorterStemmer()
 
 # Create your views here.
 import pickle
@@ -11,12 +17,35 @@ with open('app/vectorizer.pkl', 'rb') as f:
 with open('app/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
+def transform_text(text):
+    text=text.lower()
+    text=nltk.word_tokenize(text)
+    y=[]
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text=y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text=y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+    return " ".join(y)
+            
+
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
         text = request.POST['text']
-        text_vectorized = vectorizer.transform([text])
+        transformedtext=transform_text(text)
+        text_vectorized = vectorizer.transform([transformedtext])
         prediction = model.predict(text_vectorized)[0]
-        return render(request, 'index.html', {'prediction': prediction})
         print(prediction)
+        return render(request, 'index.html', {'prediction': prediction})
+        
     return render(request, 'index.html')
